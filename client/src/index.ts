94a3b1df1ee83a3609e90ca772b7c597a17c6f4d
shcherbak <shcherbak.ssa@gmail.com>
2023-02-06 18:00:16 +0100

    init: setup client

---
+++ client/src/index.ts
@@ -0,0 +1,3 @@
+import { renderApp } from 'view/main';
+
+renderApp();
---
+++ client/tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    screens: {
+      'xs': '475px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+    },
+  },
+  plugins: [],
+};
