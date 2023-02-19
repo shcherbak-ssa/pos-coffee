70fe2d92f27f3efebd22d3738c9657d5f90aa621
shcherbak <shcherbak.ssa@gmail.com>
2023-02-19 02:25:11 +0100

    feat: add user menu to app header

--- client/src/modules/admin/view/containers/AppHeaderContainer.tsx
+++ client/src/modules/admin/view/containers/AppHeaderContainer.tsx
@@ -1,11 +1,18 @@
+import { MouseEvent, useRef } from 'react';
+import type { MenuItem } from 'primereact/menuitem';
+import { Menu } from 'primereact/menu';
 import { PrimeIcons } from 'primereact/api';
 
 import type { UserSchema } from 'shared/types';
-import { APP_NAME } from 'shared/constants';
+import { APP_NAME, LocalStorageKey, ROOT_PAGE_PATH } from 'shared/constants';
+import { LocalStorage } from 'shared/helpers/local-storage';
+import { replaceLocation } from 'shared/utils/replace-location';
+import { type NavigateFunctionHook, useNavigateWithParams } from 'view/hooks/navigate';
 import { IconButton } from 'view/components/IconButton';
 
 import type { AppComponentProps } from '@admin/shared/types';
-import { UsersPhoto } from '../components/UsersPhoto';
+import { PagePath } from '@admin/shared/constants';
+import { UsersPhoto } from '@admin/view/components/UsersPhoto';
 
 export type Props = AppComponentProps & {
   currentUser: UserSchema;
@@ -13,8 +20,47 @@ export type Props = AppComponentProps & {
 
 export function AppHeaderContainer({ isAppMenuOpen, appController, currentUser }: Props) {
 
+  const userMenu = useRef<Menu>(null);
+
+  const toInfoPage: NavigateFunctionHook = useNavigateWithParams(PagePath.USERS_INFO);
+  const toEditPage: NavigateFunctionHook = useNavigateWithParams(PagePath.USERS_EDIT);
+
+  const userMenuItems: MenuItem[] = [
+    {
+      label: 'View details',
+      icon: PrimeIcons.EYE,
+      command: () => {
+        toInfoPage({ id: currentUser.id });
+      },
+    },
+    {
+      label: 'Edit',
+      icon: PrimeIcons.PENCIL,
+      command: () => {
+        toEditPage({ id: currentUser.id });
+      },
+    },
+    { separator: true },
+    {
+      label: 'Logout',
+      icon: PrimeIcons.SIGN_OUT,
+      command: () => {
+        LocalStorage.remove(LocalStorageKey.USER_TOKEN);
+        replaceLocation(ROOT_PAGE_PATH);
+      },
+    },
+  ];
+
   function toggleAppMenu(): void {
     appController.setIsAppMenuOpen(!isAppMenuOpen);
+  }
+
+  function toggleUserMenu(e: MouseEvent): void {
+    e.preventDefault();
+
+    if (userMenu.current) {
+      userMenu.current.toggle(e);
+    }
   }
 
   return (
@@ -34,12 +80,18 @@ export function AppHeaderContainer({ isAppMenuOpen, appController, currentUser }
         <h1 className="app-name text-2xl">{ APP_NAME }</h1>
       </div>
 
-      <div className="flex items-center click">
+      <div className="flex items-center click" onClick={toggleUserMenu}>
         <UsersPhoto
           size="large"
           photo={currentUser.photo}
         />
       </div>
+
+      <Menu
+        model={userMenuItems}
+        ref={userMenu}
+        popup
+      />
     </header>
   );
 
